5e57e5d541579db2a9b9773a1a4afb7aef4b6007
uygarpolat <uygarpolat@gmail.com>
2024-12-10 20:35:10 +0200

    day12 part01

--- day12/day12.py
+++ day12/day12.py
@@ -1,11 +1,54 @@
+def is_valid_arrangement(logs, nums):
+    comparison_list = []
+    count = 0
+    for log in logs:
+        if len(comparison_list) > len(nums) or comparison_list != nums[:len(comparison_list)]:
+            return 0
+        if log == '#':
+            count += 1
+        else:
+            if count != 0:
+                comparison_list.append(count)
+            count = 0
+    comparison_list.append(count)
+    comparison_list = [x for x in comparison_list if x != 0]
+    if comparison_list == nums:
+        return 1
+    else:
+        return 0
+
+def count_valid_arrangement(logs, nums):
+    
+    from itertools import product
+
+    total = 0
+    count = 0
+    q_mark = logs.count('?')
+    groups = list(product('#.', repeat=q_mark))
+
+    for group in groups:
+        temp = []
+        k = 0
+        for j in range(len(logs)):
+            if logs[j] == '?':
+                temp.append(group[k])
+                k += 1
+            else:
+                temp.append(logs[j])
+        total = is_valid_arrangement(temp, nums)
+        count += total
+    return count
+
 def main():
-    file_path = "input2.txt"
+    file_path = "input.txt"
+    valid = 0
     with open(file_path, 'r') as file:
         for line in file:
-            part1, part2 = line.split()
+            logs, part2 = line.split()
+            logs = list(logs)
             nums = list(map(int, part2.split(",")))
-            print(nums)
-
+            valid += count_valid_arrangement(logs, nums)
+    print (valid)
 
 if __name__ == "__main__":
     main()
