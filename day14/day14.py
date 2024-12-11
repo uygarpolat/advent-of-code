02398b3096507f1d09785516c841719d949dc1e5
uygarpolat <uygarpolat@gmail.com>
2024-12-12 01:08:52 +0200

    day14

--- day14/day14.py
+++ day14/day14.py
@@ -10,44 +10,93 @@ def sort_row(row):
     sorted_list = sorted(row, key=lambda x: x != 'O')
     return(sorted_list)
 
-def divide_row(row):
+def modify_row(row):
 
     replacement_row = []
 
     while(row):
         if '#' in row:
             index = row.index('#')
-            # print(f"Found # at index {index}")
             first_part = row[:index]
-            # print(f"first_part is {first_part}")
             replacement_row.extend(sort_row(first_part))
             replacement_row.append('#')
-            row = row[index+1:] # Second part
-            # print(f"second_part is {row}")
+            row = row[index+1:]
         else:
             replacement_row.extend(sort_row(row))
             row = []
 
     return replacement_row
 
+def rot_90_c_clock(grid):
+    return list(zip(*grid))[::-1]
+
+def rot_90_clock(grid):
+    return list(zip(*grid[::-1]))
+
+def rot_180(grid):
+    grid = rot_90_c_clock(grid)
+    return rot_90_c_clock(grid)
+
+def push_o_to_left(grid):
+    for i in range(len(grid)):
+        grid[i] = modify_row(grid[i])
+    return grid
+
+def calculate_load(grid):
+    count = 0
+    for i, row in enumerate(grid):
+        count += row.count('O') * (len(grid) - i)
+    return count
+
+def tilt_north(grid):
+    grid = rot_90_c_clock(grid)
+    grid = push_o_to_left(grid)
+    grid = rot_90_clock(grid)
+    return grid
+
+def tilt_west(grid):
+    grid = push_o_to_left(grid)
+    return grid
+
+def tilt_east(grid):
+    grid = rot_180(grid)
+    grid = push_o_to_left(grid)
+    grid = rot_180(grid)
+    return grid
+
+def tilt_south(grid):
+    grid = rot_90_clock(grid)
+    grid = push_o_to_left(grid)
+    grid = rot_90_c_clock(grid)
+    return grid
+
+def do_cycle(grid):
+    grid = tilt_north(grid)
+    grid = tilt_west(grid)
+    grid = tilt_south(grid)
+    grid = tilt_east(grid)
+    return grid
+
 def main():
     file_path = "input.txt"
     with open(file_path, 'r') as file:
         grid = [list(line.strip()) for line in file]
-        # grid_rotated_90_degree_clockwise = list(zip(*grid[::-1]))
-        grid_rotated_90_degree_counterclockwise = list(zip(*grid))[::-1]
-        # print_grid(grid)
-        # print("----")
-        for i in range(len(grid_rotated_90_degree_counterclockwise)):
-            grid_rotated_90_degree_counterclockwise[i] = divide_row(
-                grid_rotated_90_degree_counterclockwise[i])
-        grid2 = list(zip(*grid_rotated_90_degree_counterclockwise[::-1]))
-        # print_grid(grid2)
 
-        count = 0
-        for i, row in enumerate(grid2):
-            count += row.count('O') * (len(grid) - i)
-        print(count)
+    grid2 = grid
+    grid2 = tilt_north(grid2)
+    print(f"Solution for Part 1: {calculate_load(grid2)}")
+
+    cycle_results = []
+    iterations = 1000000000
+    for i in range(iterations):
+        grid = do_cycle(grid)
+        if grid in cycle_results:
+            index = cycle_results.index(grid)
+            revert = (((iterations) - index - 1) % (i - index)) + index + 1 - 1
+            grid = cycle_results[revert]
+            break
+        cycle_results.append(grid)
+    print(f"Solution for Part 2: {calculate_load(grid)}")
 
 if __name__ == "__main__":
     main()
