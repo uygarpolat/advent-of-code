1c5526f2b2a88ab45727a9211e6548b48b7e44e5
uygarpolat <uygarpolat@gmail.com>
2024-12-14 20:14:29 +0200

    day14

--- day14/day14.py
+++ day14/day14.py
@@ -2,11 +2,23 @@ from dataclasses import dataclass
 from collections import defaultdict
 from functools import reduce
 from operator import mul
+from collections import Counter
 
 @dataclass
 class Robot:
     pos: list[int]
     vel: tuple[int, int]
+
+def generate_grid(final_state, x_len, y_len):
+    grid = []
+    for col in range(y_len):
+        grid.append([])
+        for row in range(x_len):
+            if (row,col) in final_state:
+                grid[col].append(final_state.count((row,col)))
+            else:
+                grid[col].append('.')
+    return grid
 
 def is_in_grid(grid, loc):
     rows = len(grid)
@@ -26,7 +38,6 @@ def print_grid(grid):
         print("")
 
 def execute_move(pos, vel, x_len, y_len):
-
     new_pos_raw = tuple(map(sum, zip(pos, vel)))
     new_pos = (new_pos_raw[0] % x_len, new_pos_raw[1] % y_len)
     return new_pos
@@ -48,9 +59,7 @@ def calculate_quadrant(final_state, x_len, y_len):
                 quads[2] += 1
             else:
                 quads[3] += 1
-    # print(quads.values())
     return reduce(mul, quads.values(), 1)
-
 
 def main():
     file_path = "input.txt"
@@ -59,7 +68,6 @@ def main():
         for i, line in enumerate(file):
             pos, vel = [tuple(map(int, part.lstrip('vp=').split(','))) for part in line.split()]
             robots[i] = Robot(list(pos), vel)
-            # print(robots[i].pos[0])
 
     part_one_solution = 0
     x_len = 101
@@ -69,32 +77,21 @@ def main():
     for i in range(iter):
         final_state = []
         for j in robots:
-            # print(f"pos: {robots[j].pos}, vel: {robots[j].vel}")
             pos = robots[j].pos
             vel = robots[j].vel
             robots[j].pos = execute_move(pos, vel, x_len, y_len)
-            # print(robots[j].pos)
         for k in robots:
             final_state.append(robots[k].pos)
         if i == 99:
             part_one_solution = calculate_quadrant(final_state, x_len, y_len)
-        if i == 6531:
-            display_state(final_state, x_len, y_len)
-            print(f"Christmas tree found after {i+1} seconds")
-    print(f"Solution for Part 1: {part_one_solution}")
-    print(f"Solution for Part 2: {6532}")
-
-def display_state(final_state, x_len, y_len):
-    grid = []
-    for col in range(y_len):
-        grid.append([])
-        for row in range(x_len):
-            if (row,col) in final_state:
-                grid[col].append(final_state.count((row,col)))
-            else:
-                grid[col].append('.')
-    print_grid(grid)
-    print("-----------------------------------------------")
+            print(f"Solution for Part 1: {part_one_solution}")
+        else:
+            maximum_x = max(Counter(x[0] for x in final_state).values())
+            maximum_y = max(Counter(y[1] for y in final_state).values())
+            if maximum_x > x_len / 4 and maximum_y > y_len / 4:
+                print(f"Solution for Part 2: {i+1}")
+                if i > 99:
+                    return
 
 if __name__ == "__main__":
     main()
