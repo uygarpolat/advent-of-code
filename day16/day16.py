2d6511308670ab6fafa1d0287fda7fe7eeac9e3f
Uygar Polat <upolat@c1r5p15.hive.fi>
2024-12-16 15:20:00 +0200

    day16

--- day16/day16.py
+++ day16/day16.py
@@ -1,3 +1,7 @@
+from collections import deque
+from collections import defaultdict
+
+
 def print_grid(grid, flag=1):
     rows = len(grid)
     cols = len(grid[0])
@@ -14,18 +18,16 @@ def print_grid(grid, flag=1):
             print("")
     return loc_start, loc_end
 
-def traverse(grid, loc_cur, loc_end, old_dir):
+
+
+
+def traverse(grid, loc_cur, loc_end, old_dir, logbook):
     print(f"Testing {loc_cur}")
     dirs = [(1,0),(0,1),(-1,0),(0,-1)]
-    bool_val = False
 
     value_loc_cur = grid[loc_cur[0]][loc_cur[1]]
     value_loc_end = grid[loc_end[0]][loc_end[1]]
-
-    if value_loc_cur == '#':
-        return False
-    # elif value_loc_cur == value_loc_end:
-    #     return 1, True
+    loc_cur_logbook_value = logbook[loc_cur]
     
     for new_dir in dirs:
         boost = 1
@@ -33,27 +35,39 @@ def traverse(grid, loc_cur, loc_end, old_dir):
             continue
         if new_dir[0] != old_dir[0] or new_dir[1] == old_dir[1]:
             boost == 1000
+        else:
+            boost == 0
         
         loc_new = tuple(map(sum, zip(loc_cur, new_dir)))
         value_loc_new = grid[loc_new[0]][loc_new[1]]
+        loc_new_logbook_value = logbook[loc_new]
 
         if value_loc_new == '#':
             continue
-        elif value_loc_new == value_loc_end:
-            return loc_new
+
+        
+        if value_loc_new == value_loc_end:
+            logbook[loc_new].append(loc_cur_logbook_value + loc_new_logbook_value)
+            return
         else:
-            bool_val = traverse(grid, loc_new, loc_end, new_dir)
-    return bool_val
+            if logbook[loc_new] > boost + 1 + loc_cur_logbook_value:
+                logbook[loc_new] = boost + 1 + loc_cur_logbook_value
+                traverse(grid, loc_new, loc_end, new_dir, logbook)
+    return
 
 def main():
-    file_path = "input2.txt"
+    file_path = "input3.txt"
     with open(file_path, 'r') as file:
         grid = [list(line.strip()) for line in file]
     loc_cur, loc_end = print_grid(grid, 0)
     print(loc_cur, loc_end)
 
-    score = traverse(grid, loc_cur, loc_end, (0,1))
-    print(score)
+    logbook = defaultdict(lambda: 1000000000)
+    logbook[loc_cur] = 0
+    logbook[loc_end] = []
+
+    traverse(grid, loc_cur, loc_end, (0,1), logbook)
+    print(logbook)
 
 if __name__ == "__main__":
     main()
