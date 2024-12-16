6e490dea1cb0ef78cc2aee4da293d9f94d6cce3f
uygarpolat <uygarpolat@gmail.com>
2024-12-16 17:49:06 +0200

    day16

--- day16/day16.py
+++ day16/day16.py
@@ -1,5 +1,8 @@
 from collections import deque
 from collections import defaultdict
+import sys
+
+sys.setrecursionlimit(10000)
 
 
 def print_grid(grid, flag=1):
@@ -21,42 +24,40 @@ def print_grid(grid, flag=1):
 
 
 
-def traverse(grid, loc_cur, loc_end, old_dir, logbook):
-    print(f"Testing {loc_cur}")
+def traverse(grid, loc_cur, loc_end, old_dir, logbook, best_set):
     dirs = [(1,0),(0,1),(-1,0),(0,-1)]
 
-    value_loc_cur = grid[loc_cur[0]][loc_cur[1]]
     value_loc_end = grid[loc_end[0]][loc_end[1]]
     loc_cur_logbook_value = logbook[loc_cur]
     
     for new_dir in dirs:
-        boost = 1
+        boost = 0
         if new_dir[0] + old_dir[0] == 0 and new_dir[1] + old_dir[1] == 0:
             continue
-        if new_dir[0] != old_dir[0] or new_dir[1] == old_dir[1]:
-            boost == 1000
+        if new_dir[0] != old_dir[0] and new_dir[1] != old_dir[1]:
+            boost = 1000
         else:
-            boost == 0
+            boost = 0
         
         loc_new = tuple(map(sum, zip(loc_cur, new_dir)))
         value_loc_new = grid[loc_new[0]][loc_new[1]]
-        loc_new_logbook_value = logbook[loc_new]
 
         if value_loc_new == '#':
             continue
 
-        
         if value_loc_new == value_loc_end:
-            logbook[loc_new].append(loc_cur_logbook_value + loc_new_logbook_value)
+            if logbook[loc_new] > boost + 1 + loc_cur_logbook_value:
+                best_set.add(loc_new)
+                logbook[loc_new] = boost + 1 + loc_cur_logbook_value
             return
         else:
             if logbook[loc_new] > boost + 1 + loc_cur_logbook_value:
                 logbook[loc_new] = boost + 1 + loc_cur_logbook_value
-                traverse(grid, loc_new, loc_end, new_dir, logbook)
+                traverse(grid, loc_new, loc_end, new_dir, logbook, best_set)
     return
 
 def main():
-    file_path = "input3.txt"
+    file_path = "input.txt"
     with open(file_path, 'r') as file:
         grid = [list(line.strip()) for line in file]
     loc_cur, loc_end = print_grid(grid, 0)
@@ -64,10 +65,12 @@ def main():
 
     logbook = defaultdict(lambda: 1000000000)
     logbook[loc_cur] = 0
-    logbook[loc_end] = []
+    best_set = set()
+    best_set.add(loc_cur)
 
-    traverse(grid, loc_cur, loc_end, (0,1), logbook)
-    print(logbook)
+    traverse(grid, loc_cur, loc_end, (0,1), logbook, best_set)
+    print(f"Solution for Part 1: {logbook[loc_end]}")
+    print(len(best_set))
 
 if __name__ == "__main__":
     main()
