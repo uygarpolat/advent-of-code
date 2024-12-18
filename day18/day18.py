a4e0851ffa0fa04e126962e0b0d40d9c9886d2fe
Uygar Polat <upolat@c2r5p6.hive.fi>
2024-12-18 13:31:50 +0200

    day18

--- day18/day18.py
+++ day18/day18.py
@@ -10,30 +10,19 @@ def is_in_grid(grid_size, loc):
         return True
     return False
 
-def print_grid(grid_size, first_n_coordinates):
-    rows = grid_size
-    cols = grid_size
-    for row in range(cols):
-        for col in range(rows):
-            if (col,row) in first_n_coordinates:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print("")
-
 def main():
     file_path = "input.txt"
     with open(file_path, 'r') as file:
         coordinates = tuple(tuple(int(num) for num in line.strip().split(',')) for line in file)
-    # print(coordinates)
     
+    defaultdict_value = 1000000
     dirs = [(1,0),(0,1),(-1,0),(0,-1)]
     grid_size = 71
-    for i in range(1, len(coordinates), 1):
-        # Fix logic
+    for i in range(1024, len(coordinates), 1):
         first_n = i
         first_n_coordinates = coordinates[:first_n]
-        logbook = defaultdict(lambda: 1000000)
+        logbook = defaultdict(lambda: defaultdict_value)
+        logbook.clear()
         pq = PriorityQueue()
         logbook[(0,0)] = 0
         pq.put((logbook[(0,0)], (0,0)))
@@ -47,9 +36,15 @@ def main():
                 if logbook[new_loc] > priority + 1:
                     logbook[new_loc] = priority + 1
                     pq.put((logbook[new_loc], new_loc))
-        print(logbook[(grid_size - 1, grid_size - 1)])
-
-    
+                if logbook[(grid_size - 1, grid_size - 1)] != defaultdict_value:
+                    pq.empty()
+                    break
+        local_solution = logbook[(grid_size - 1, grid_size - 1)]
+        if i == 1024:
+            print(f"Solution for Part 1: {local_solution}")
+        if local_solution == defaultdict_value:
+            print(f"Solution for Part 2: {",".join(map(str, first_n_coordinates[-1]))}")
+            return    
 
 if __name__ == "__main__":
     main()
