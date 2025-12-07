62e2ab4c4543e89977f02224173fe0feb4f881a8
uygarpolat <uygarpolat@gmail.com>
2025-12-07 15:56:17 +0200

    2025 day07

--- advent-of-code-2025/day07/day09.py
+++ advent-of-code-2025/day07/day09.py
@@ -7,33 +7,20 @@ def main():
     with open(filepath, "r") as f:
         grid = [list(line.strip()) for line in f]
 
-    rows = len(grid)
-    cols = len(grid[0])
+    rows, cols, result1 = len(grid), len(grid[0]), 0
     start = (0, grid[0].index("S"))
-    locs = set([start[1]])
     tally = defaultdict(int, {start[1]: 1})
-    result1 = 0
 
     for i in range(start[0] + 1, rows):
         for j in range(cols):
-            if grid[i][j] == "^" and j in locs:
-
-                if j - 1 >= 0:
-                    locs.add(j - 1)
-                    tally[j - 1] += tally[j]
-
-                if j + 1 <= cols - 1:
-                    locs.add(j + 1)
-                    tally[j + 1] += tally[j]
-
-                locs.remove(j)
-                tally[j] = 0
+            if grid[i][j] == "^" and j in tally:
                 result1 += 1
-
-    result2 = sum(tally.values())
+                tally[j - 1] += tally[j]
+                tally[j + 1] += tally[j]
+                del tally[j]
 
     print(f"Solution for Part 1: {result1}")
-    print(f"Solution for Part 2: {result2}")
+    print(f"Solution for Part 2: {sum(tally.values())}")
 
 
 if __name__ == "__main__":
